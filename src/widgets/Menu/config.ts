10ce56eae2d4d70085379bb90c824ead0b1a20e0
onoja64 <corneliussamuel64@gmail.com>
2021-08-28 23:25:13 +0100

    commit1

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,93 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.boomcakeswap.finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.boomcakeswap.finance/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pancakeswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pancakeswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pancakeswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pancakeswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/boomcakeDefi/",
+      },
+      {
+        label: "Docs",
+        href: "https://boomcakeswap.gitbook.io/boomcake-swap/",
+      },
+      {
+        label: "Blog",
+        href: "https://boomcake-swap.medium.com/",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/boomcaketokenbsc",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/boomcaketokenbsc",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/boomcaketokenbsc",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
